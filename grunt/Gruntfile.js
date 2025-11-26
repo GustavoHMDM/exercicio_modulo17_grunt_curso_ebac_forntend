f1be749f400b4699b8def3b36fbfbcecf2da91a2
GustavoHMDM <gustavohmdm180506@gmail.com>
2025-11-26 12:20:45 -0300

    fourth commit

---
+++ grunt/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files : {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+
+    grunt.registerTask('default', ['less:production', 'uglify', 'htmlmin']);
+}
